8abfb4ed291adf5b9773cb528294ea182296959a
Ken <kennethkopelson@gmail.com>
2021-12-01 11:14:39 -0800

    added html, css, and javascript for restaurant menu page.

---
+++ Restaurant Menu Page/script.js
@@ -0,0 +1,75 @@
+// Key concepts covered:
+
+// arrays
+// objects
+// forEach()
+// DOMContentLoaded
+// map, reduce, and filter
+// innerHTML
+// includes method
+
+const menu = {
+    breakfast: [
+        {
+            title: "Buttermilk Pancakes",
+            price: "$15.99",
+            description: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+            image: "https://bakerbynature.com/wp-content/uploads/2016/02/IMG_8456-6-2.jpg"
+        },
+        {
+            title: "Country Delight",
+            price: "$20.99",
+            description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austing mlkshk truffaunt",
+            image: "http://cdn.theculturetrip.com/wp-content/uploads/2016/06/img_3618.jpg"
+        },
+        {
+            title: "Bacon Overflow",
+            price: "$8.99",
+            description: "",
+            image: "https://media.istockphoto.com/photos/cooked-greasy-bacon-picture-id160860037"
+        }
+
+    ],
+    lunch: [
+        {
+            title: "Diner Double",
+            price: "$15.99",
+            description: "",
+            image: "https://eu2prddennyscdnstr.blob.core.windows.net/cms/menu/product/americas-diner-double_thumbnailmedium_2019-06-12-22-59-23.jpg"
+        },
+        {
+            title: "Egg Attack",
+            price: "$15.99",
+            description: "",
+            image: "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2016_02/933116/breakfast_pizza_bianca.jpg"
+        },
+        {
+            title: "Buttermilk Pancakes",
+            price: "$15.99",
+            description: "",
+            image: "https://bakerbynature.com/wp-content/uploads/2016/02/IMG_8456-6-2.jpg"
+        }
+    ],
+    shakes: [
+        {
+            title: "Godzilla Milkshake",
+            price: "$6.99",
+            description: "",
+            image: "https://i0.wp.com/miakouppa.com/wp-content/uploads/2019/06/Milkshake-edited-4.jpg?resize=656%2C875&ssl=1"
+        },
+        {
+            title: "Oreo Dream",
+            price: "$18.99",
+            description: "",
+            image: "http://www.sweetdscreations.com/blog/wp-content/uploads/2017/07/img_9903.jpg"
+        },
+        {
+            title: "Quarantine Buddy",
+            price: "$16.99",
+            description: "",
+            image: "https://www.thespruceeats.com/thmb/to4ToJyiIm0DcxElULigDHKTOPY=/1500x1041/filters:fill(auto,1)/chocolatemilkshake-5c6e0b9e46e0fb0001e6e84d.jpg"
+        },
+    ]
+}
+
+console.log(menu);
