bbbf10e351daa1748d25fc59f53fe8f0d28b347b
Ken <kennethkopelson@gmail.com>
2021-12-21 14:41:21 -0800

    initialized files for simon game, added basic html and css

---
+++ Simon Game/script.js
@@ -0,0 +1,17 @@
+// LQKey concepts covered:
+
+// querySelector()
+// addEventListener()
+// setInterval()
+// clearInterval()
+// setTimeout()
+// play()
+// Math.floor()
+// Math.random()
+
+function handleClick(e){
+    console.log(e.target)
+}
+
+
+document.addEventListener("click", handleClick);
